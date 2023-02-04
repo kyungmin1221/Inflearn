efb0194bacee6e5f1d29811732a19b80b7c90c14
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-02-04 14:55:07 +0900

    유효한 팰린트롬

--- Inflearn8.java
+++ Inflearn8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 // 펠린드롬 ( replaceAll() 정규식 문제 )
 // 팰린드롬 : 앞에서 읽을때나 뒤에서 읽을 때나 같은 문자열을 말함
 // 주어진 문자열이 팰린드롬인지 확인하는 문제
-// replaceAll(pattern , str) -> pattern(정규표현식) 과 일치히는 문자열들을 str(문자열) 로 바꾸어줌  
+// replaceAll(pattern , str) -> pattern(정규표현식) 과 일치히는 문자열들을 str(문자열) 로 바꾸어줌
 public class Inflearn8 {
     public String solution(String str) {
         String answer = "NO";
