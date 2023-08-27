ae41c98263f3d4ba39d4a98c250f20c3a9d27f6a
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-08-27 22:24:49 +0900

    좌표정렬

---
+++ Inflearn49.java
@@ -0,0 +1,23 @@
+package Inflearn.Inflearn;
+
+// 좌표정렬 //
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Inflearn49 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            String str = st.nextToken();
+            String str2 = st.nextToken();
+        }
+
+
+    }
+}
