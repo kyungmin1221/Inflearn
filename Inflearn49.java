f3ac1da38b7a040a047337c823e072f63bacb3c8
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-08-28 17:40:56 +0900

    좌표정렬 ( Comparable & Comparator )

--- Inflearn49.java
+++ Inflearn49.java
@@ -2,15 +2,13 @@ package Inflearn.Inflearn;
 
 // 좌표정렬 //
 
-// Comparable<T>
+// Comparable 사용
 // a - b 가 음수이면 오름차순 정렬을 의미한다. 즉 , a b ... ~~
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class MyComparable implements Comparable<MyComparable> {
     public int x,y;
@@ -45,3 +43,48 @@ public class Inflearn49 {
         }
     }
 }
+
+
+// 좌표정렬 //
+
+// Comparator 사용
+// a - b 가 음수이면 오름차순 정렬을 의미한다. 즉 , a b ... ~~
+
+
+//class MyComparator{
+//    public int x,y;
+//    MyComparator(int x,int y) {
+//        this.x = x;
+//        this.y= y;
+//    }
+//    public String toString() {
+//        return x + " " + y;
+//    }
+//}
+//
+//public class Inflearn49 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(bf.readLine());
+//        List<MyComparator> A = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            StringTokenizer st = new StringTokenizer(bf.readLine());
+//            int x = Integer.parseInt(st.nextToken());
+//            int y = Integer.parseInt(st.nextToken());
+//            A.add(new MyComparator(x, y));
+//        }
+//
+//        Collections.sort(A, new Comparator<MyComparator>() {
+//            @Override
+//            public int compare(MyComparator o1, MyComparator o2) {
+//                if (o1.x == o2.x) {
+//                    return o1.y - o2.y;
+//                }
+//                return o1.x - o2.x;
+//            }
+//        });
+//        for (MyComparator o : A) {
+//            System.out.println(o);
+//        }
+//    }
+//}
